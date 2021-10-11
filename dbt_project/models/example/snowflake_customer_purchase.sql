96d21e8f93ef5302b9046f8ecf82c68ce8367d61
Iyanumanuel <e.iyanuloluwa@gmail.com>
2021-10-11 13:10:39 +0100

    added sources

--- dbt_project/models/example/snowflake_customer_purchase.sql
+++ dbt_project/models/example/snowflake_customer_purchase.sql
@@ -6,8 +6,8 @@ SELECT
     c.c_name,
     c.c_nationkey as nation,
     SUM(o.o_totalprice) as total_price
-FROM "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."CUSTOMER" c
-LEFT JOIN "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS" o
+FROM {{ source('sample', 'customer') }} c
+LEFT JOIN FROM {{ source('sample', 'orders') }} c o
 ON c.c_custkey = o.o_custkey
 GROUP BY
     c.c_custkey,
