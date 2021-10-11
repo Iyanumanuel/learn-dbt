ffa34c2dd2f8854b474a0794854bae319a441798
Iyanumanuel <e.iyanuloluwa@gmail.com>
2021-10-11 13:37:12 +0100

    macros

--- dbt_project/macros/renaming_segments.sql
+++ dbt_project/macros/renaming_segments.sql
@@ -1,10 +1,10 @@
-{% macro rename_segments(column_name) % }
+{% macro rename_segments(column_name) %}
     CASE    
         WHEN {{column_name}} in ('BUILDING', 'HOUSEHOLD', 'FURNITURE')
             THEN 'segment_1'
         ELSE 'segment_2'
     END
- {% endmacro %}
+{% endmacro %}
 
 
 
