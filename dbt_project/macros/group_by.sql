0ee0320f4fa6e03b5a593036def8aee977756277
Iyanumanuel <e.iyanuloluwa@gmail.com>
2021-10-11 13:38:45 +0100

    macros

--- dbt_project/macros/group_by.sql
+++ dbt_project/macros/group_by.sql
@@ -3,8 +3,8 @@
         {% for i in range (1, n + 1) %}
             {{ i }}
             {% if not loop.last %} , {% endif %}
-    {% endfor %}
- {% endmacro %}
+        {% endfor %}
+{% endmacro %}
 
 
 
